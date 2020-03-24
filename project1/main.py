cae7c16a0db1e35d3d82b6463f462f2fa20a3f2c
Nic Cejda <ncejda@gmail.com>
2020-03-23 21:45:45 -0500

    Updated findNames and findGenders

--- project1/main.py
+++ project1/main.py
@@ -7,8 +7,26 @@ import nltk
 import spacy
 from spacy import displacy
 from collections import Counter
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+from collections import OrderedDict
+from spacy.pipeline import EntityRuler
+
+
+#Had to add some exceptions b/c Spacy's model isn't perfect. These are specific to my test files in the docs/ folder.
+nlp = spacy.load("en_core_web_sm")
+ruler = EntityRuler(nlp, overwrite_ents=True)
+patterns = [{"label": "PERSON", "pattern": "Secretary of State"},
+            {"label": "PERSON", "pattern": [{"LOWER": "secretary"}, {"LOWER": "of state"}]},
+            {"label": "PERSON", "pattern": "Sanders"},
+            {"label": "PERSON", "pattern": "Peter F. Nardulli"},
+            {"label": "PERSON", "pattern": "Hillary Clinton"},
+            {"label": "ORG", "pattern": "Charlotte Hornets"}]
+
+ruler.add_patterns(patterns)
+nlp.add_pipe(ruler)
+
+myFileNames = []
+myRedactList = []
+
 
 
 #This project needs to:
@@ -24,9 +42,9 @@ nlp = en_core_web_sm.load()
 # 8 - Write tests for each method in seperate test_etc.py files.
 
 
-
+#nltk.download('punkt')
 #Need a global variable myFileNames, accessible to all the methods.
-myFileNames = []
+
 
 
 def findDocs(userglob):
@@ -48,16 +66,136 @@ def findDocs(userglob):
                 myFileNames.append(currentFilePath)
 
 
-def redactNames(document):
+def findNames(document, nameFlag):
 
-    # I guess step1 is to use NLTK on the provided document to split the string into words.
-    #Then we will classify them as Names or Not Names
-    #Then I will add all the strings to-be-redacted to a list. This can be a running list between methods.
+    if not nameFlag:
+        return
 
-    #document input must be a string.
-    tokens = nltk.word_tokenize(document)
-    print(tokens)
-    
+    # Step 1, use Spacy's en-core-web-lg module to identify Named Entites.
+    # Step 2, add all People to the Redaction list.
+    doc = nlp(document)
+
+    for ent in doc.ents:
+        #print(ent.text, ent.label_)
+        foundNP = False
+        if ent.label_ == "PERSON":
+            pattern = re.compile(ent.text)
+            for noun_chunks in doc.noun_chunks:
+                #print(noun_chunks.text)
+                if pattern.search(noun_chunks.text):
+                    myRedactList.append(noun_chunks.text)
+                    foundNP = True
+
+            if not foundNP:
+                myRedactList.append(ent.text)
+        #elif ent.label_ == "ORG":
+          #  myRedactList.append(ent.text)
+
+    #This clears out duplicates.
+    newList = list(OrderedDict.fromkeys(myRedactList))
+    myRedactList.clear()
+    for x in range(0, len(newList)):
+        myRedactList.append(newList[x])
+    #print(myRedactList)
+
+
+
+def findGenders(document, genderFlag, nameFlag):
+    if not genderFlag:
+        return
+    pass
+
+# Some of the Male and Female words were taken from
+# https://medium.com/@rajat.jain1/natural-language-extraction-using-spacy-on-a-set-of-novels-88b159d68686
+# and https://www.thefreedictionary.com/List-of-pronouns.htm
+
+
+    female_words = ["she", "her", "hers", "herself", "actress", "bachelorette", "empress", "queen", "heroine", "hostess",
+                'mrs', 'ms', 'miss', 'lady', 'madameoiselle', 'baroness', 'mistress', 'queen',
+                'princess', 'madam', 'madame', "landlady", "stewardess", "waitress", "girl", "bride", "sister", "mum",
+                "mom", "mommy", "duchess", "woman", "mother", "goddess", "grandmother", "heiress", "heroine", "wife",
+                "queen", "babe", "mistress", "niece", "policewoman", "saleswoman", "princess", "daughter", "aunt",
+                "auntie", "witch", "grandma", "lass", "lassie", "girlie"]
+
+    male_words = ['mr', 'sir', 'monsieur', 'captain', 'chief', 'master', 'lord', 'baron', 'mister', 'prince', 'king',
+              "he", "him", "his", "himself", "actor", "bachelor", "emperor", "hero", "host", "landlord", "steward",
+              "waiter", "fireman", "policeman", "mailman", "salesman", "boy", "male", "man", "dad", "daddy", "duke",
+              "father", "god", "grandfather", "heir", "husband", "master", "nephew", "prince", "son", "uncle", "wizard",
+              "grandpa", "pa", "pappy", "warlock", "lad", "laddie", "chap", "fella", "dude", "bro"]
+
+    # Assume all names are Gendered, or would giveaway the gender. Just call findNames here to be safe.
+    # No need to call it if its already been called.
+    if not nameFlag:
+        findNames(document, True)
+
+    for word in female_words:
+        myStr = r"\b" + word + r"\b"
+        pattern = re.compile(myStr)
+        if pattern.search(document):
+            myRedactList.append(word)
+
+    for word in male_words:
+        myStr = r"\b" + word + r"\b"
+        pattern = re.compile(myStr)
+        if pattern.search(document):
+            myRedactList.append(word)
+
+    #Now time for some clean-up.
+    # This clears out duplicates.
+    newList = list(OrderedDict.fromkeys(myRedactList))
+    myRedactList.clear()
+    for x in range(0, len(newList)):
+        myRedactList.append(newList[x])
+    #print(myRedactList)
+
+
+
+
+
+
+
+def findDates(document, dateFlag):
+    if not dateFlag:
+        return
+    pass
+
+def findConcepts(document, conceptFlag):
+    if not conceptFlag:
+        return
+    pass
+
+def printDoc(document):
+    pass
+
+def runStats():
+    pass
+
+def redact(document):
+
+    # We sort our redactList by length. This ensures that the longer names like "President Donald Trump" get redacted
+    # before "Trump". This is important because we are doing the replacements with regex, so if "Trump" is turned into
+    # \u2588 then regex, won't find the longer form if the shorter form was redacted first. Not the most elegant
+    # solution, but it works for now.
+
+    sortRedact = sorted(myRedactList, key=len, reverse = True)
+    print(sortRedact)
+
+
+    for x in range(0,len(sortRedact)):
+
+        sortRedact[x] = re.escape(sortRedact[x])
+        myStr = r"\b" + sortRedact[x] + r"\b"
+        pattern = re.compile(myStr)
+        document = pattern.sub("\u2588", document)
+
+    #If two redactions occur next to each other, turn it into one block. This helps further protect our document.
+    twoBlocks = re.compile("\u2588" + " " + "\u2588")
+    document = twoBlocks.sub("\u2588", document)
+
+    return document
+
+
+
 
 
 
@@ -87,26 +225,26 @@ if __name__ == '__main__':
     # What I want is a list of document names. I can accept an input *.txt, and I want to find all the strings
     # "news1, news2, etc. and make them into a list, which I can then cycle through when using nltk and spacy.
 
+    findNamesFlag = True
+    findGendersFlag = True
+
     fileext = "*.txt"
     findDocs(fileext)
     print(myFileNames)
 
     if(myFileNames):
-        myNews1 = open(myFileNames[1], 'r', encoding="UTF-8")
+        myNews1 = open(myFileNames[2], 'r', encoding="UTF-8")
         myNews = myNews1.read()
         myNews1.close()
 
-    redactNames(myNews)
+    findNames(myNews, findNamesFlag)
+    findGenders(myNews, findGendersFlag, findNamesFlag)
+    redactNews = redact(myNews)
 
     block = "\u2588"
-    #match = re.compile("the ")
-    #match2 = re.compile("coronavirus")
-    #match3 = re.compile(block + " " + block)
-    #redactNews = match.sub(block + " ", myNews)
-    #redactNews = match2.sub(block, redactNews)
-    #redactNews = match3.sub(block, redactNews)
-    print(myNews)
-    #print(redactNews)
+
+
+    print(redactNews)
 
 
 
