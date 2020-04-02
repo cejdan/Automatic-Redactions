32ff666f18d9126f5236f06e070aa90ebde355d8
Nic Cejda <ncejda@gmail.com>
2020-04-02 17:12:44 -0500

    Got findConcepts working and fixed tons of bugs

--- project1/main.py
+++ project1/main.py
@@ -15,10 +15,11 @@ from spacy.matcher import Matcher
 from collections import Counter
 from collections import OrderedDict
 from spacy.pipeline import EntityRuler
+import pandas as pd
 
 
 #Had to add some exceptions b/c Spacy's model isn't perfect. These are specific to my test files in the docs/ folder.
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_md")
 matcher = Matcher(nlp.vocab)
 ruler = EntityRuler(nlp, overwrite_ents=True)
 patterns = [{"label": "PERSON", "pattern": [{"LOWER": "secretary"}, {"LOWER": "of state"}]},
@@ -40,15 +41,14 @@ myRedactedDocs = []
 
 #This project needs to:
 # DONE 1 - Accept a *.txt or *.md input (or both) and find all the .txt or .md files in the project folder.
-# 2 - Read the file(s) and redact (Store in a list?) all words and related words to the provided flags,
+# DONE 2 - Read the file(s) and redact (Store in a list?) all words and related words to the provided flags,
 #     'names', 'genders', and 'dates'.
 # 3 - Redact all words and related words from the provided 'concept' flag(s) - can use word matrices from Spacy.
 #     1 method call for each provided concept flag.
-# 4 - Double-check sentences, redact full sentences if the meaning is still clear. How, not sure yet. Optional for now.
-# DONE 5 - Actually replace all redacted words with either a single Block U+2588 character, or multiple block characters.
-# 6 - Output the redacted files to a folder provided by the user (or default to output/ if none provided)
-# 7 - Generate a statistics function, which prints information about the run to stdout, stderr, or a file.
-# 8 - Write tests for each method in seperate test_etc.py files.
+# DONE 4 - Actually replace all redacted words with either a single Block U+2588 character, or multiple block characters.
+# 5 - Output the redacted files to a folder provided by the user (or default to output/ if none provided)
+# 6 - Generate a statistics function, which prints information about the run to stdout, stderr, or a file.
+# 7 - Write tests for each method in seperate test_etc.py files.
 
 
 #nltk.download('punkt')
@@ -96,8 +96,8 @@ def findNames(documents, nameFlag, fromGender):
     # Step 1, use Spacy's en-core-web-lg module to identify Named Entites.
     # Step 2, add all People and their token locs to the Redaction list.
 
-    for x in range(0, len(documents)):
-        doc = nlp(documents[x])
+    for i in range(0, len(documents)):
+        doc = nlp(documents[i])
         thisRedactList = []
 
         for ent in doc.ents:
@@ -128,9 +128,11 @@ def findNames(documents, nameFlag, fromGender):
 
                     thisRedactList.append(mySmallList)
 
-        if len(myRedactList) == len(documents):
-            myRedactList[x].append(mySmallList)
-        else:
+        if len(myRedactList) == len(documents) and len(thisRedactList) > 0:
+            for j in range(0,len(thisRedactList)):
+                myRedactList[i].append(thisRedactList[j])
+        #elif len(thisRedactList) > 0:
+        elif len(myRedactList) < len(documents):
             myRedactList.append(thisRedactList)
 
 
@@ -144,18 +146,20 @@ def findGenders(documents, genderFlag, nameFlag):
 # https://medium.com/@rajat.jain1/natural-language-extraction-using-spacy-on-a-set-of-novels-88b159d68686
 # and https://www.thefreedictionary.com/List-of-pronouns.htm
 
-    female_words = ["she", "her", "hers", "herself", "actress", "bachelorette", "empress", "heroine", "hostess",
-        'mrs', 'ms', 'miss', 'lady', 'madameoiselle', 'baroness', 'mistress', 'queen',
-        'madam', 'madame', "landlady", "stewardess", "waitress", "girl", "bride", "sister",
-        "mom", "mommy", "duchess", "woman", "mother", "goddess", "grandmother", "heroine", "wife",
-        "babe", "niece", "policewoman", "saleswoman", "princess", "daughter", "aunt",
-        "witch", "grandma", "female"]
+    # Pronouns, Titles,  Descriptions,  Relationships,  Royalty
+    female_words = ["she", "her", "hers", "herself",
+                    'mrs', 'ms', 'miss', 'madam', 'madame', 'lady', 'mistress',
+                    "female", "woman", "girl",
+                    "sister", "mother", "mom", "grandmother", "grandma",
+                    "wife", "niece", "daughter", "aunt",
+                    "queen", "princess"]
 
-    male_words = ["he", "him", "his", 'mr', 'sir', 'monsieur', 'captain', 'chief', 'master', 'lord', 'baron', 'mister',
-        'prince', 'king', "himself", "actor", "bachelor", "emperor", "host",
-        "waiter", "fireman", "policeman", "mailman", "salesman", "boy", "male", "man", "dad", "daddy", "duke",
-        "father", "god", "grandfather", "husband", "nephew", "son", "uncle", "wizard",
-        "grandpa", "pa", "warlock", "chap", "fella", "dude", "bro"]
+    male_words = ["he", "him", "his", "himself",
+                "mr", "mister", "sir",
+                "boy", "male", "man",
+                "dad", "daddy", "father", "grandfather", "husband", "nephew", "son", "uncle", "grandpa", "pa",
+                "bro", "brother",
+                "king", "prince"]
 
     gendered_words = female_words + male_words
     # Assume all names are Gendered, or would giveaway the gender. Just call findNames here to be safe.
@@ -167,47 +171,88 @@ def findGenders(documents, genderFlag, nameFlag):
         thisRedactList = []
         mySmallList = []
         for word in gendered_words:
-            avacadotoast = findTokenLocs(documents[i], word)
-            for j in range(0,len(avacadotoast)):
-                token_locs = [avacadotoast[j][0],avacadotoast[j][1]]
-                #print(word, avacadotoast[j][0],avacadotoast[j][1], "Gender")
+            locationlist = findTokenLocs(documents[i], word)
+            for j in range(0,len(locationlist)):
+                token_locs = [locationlist[j][0],locationlist[j][1]]
+                #print(word, locationlist[j][0],locationlist[j][1], "Gender")
                 mySmallList = [word, token_locs, "Gender"]
                 thisRedactList.append(mySmallList)
 
         if len(myRedactList) == len(documents) and len(thisRedactList) > 0:
-            myRedactList[i].append(mySmallList)
-        elif len(thisRedactList) > 0:
+            for j in range(0,len(thisRedactList)):
+                myRedactList[i].append(thisRedactList[j])
+        elif len(myRedactList) < len(documents):
             myRedactList.append(thisRedactList)
 
 
 def findDates(documents, dateFlag):
     if not dateFlag:
         return
-    pass
+
+    for i in range(0, len(documents)):
+        doc = nlp(documents[i])
+        thisRedactList = []
+        for ent in doc.ents:
+            #print(ent.text, ent.label_)
+            foundNP = False
+            if ent.label_ == "DATE":
+                #Now is the time to find out what the token locations are for this span of tokens
+                token_locs = [ent.start, ent.end]
+                mySmallList = [ent.text, token_locs, "Dates"]
+                thisRedactList.append(mySmallList)
+
+        if len(myRedactList) == len(documents) and len(thisRedactList) > 0:
+            for j in range(0,len(thisRedactList)):
+                myRedactList[i].append(thisRedactList[j])
+        elif len(myRedactList) < len(documents):
+            myRedactList.append(thisRedactList)
 
 def findConcepts(documents, concept, conceptFlag):
     if not conceptFlag:
         return
+    #Ok, need to build a similarity matrix for each word. Anything over 0.45 gets included as similar and added
+    #to the redact list.
+    conceptdoc = nlp(concept)
+    for i in range(0,len(documents)):
+        thisRedactList = []
+        doc = nlp(documents[i])
+        for j in range(0,len(doc)):
+            if doc[j].has_vector:
+                if doc[j].similarity(conceptdoc[0]) > 0.45: #I really like >0.45, it works really nicely! Very impressive!
+                    token_locs = [j, j+1]
+                    mySmallList = [doc[j].text, token_locs, str("Concept: " + concept)]
+                    thisRedactList.append(mySmallList)
+
+                    #print("Word: " + str(doc[j]) + " - Similarity to " + concept + ": " + str(doc[j].similarity(conceptdoc[0])))
+
+        if len(myRedactList) == len(documents) and len(thisRedactList) > 0:
+            for j in range(0,len(thisRedactList)):
+                myRedactList[i].append(thisRedactList[j])
+        elif len(myRedactList) < len(documents):
+            myRedactList.append(thisRedactList)
+
+
+
+def runStats(outputType, fileNames):
+
+    if outputType == "stdout":
+        for i in range(0,len(fileNames)):
+            simpleFileName = os.path.basename(fileNames[i])
+            mystats = pd.DataFrame(myRedactList[i])
+            mystats = mystats.rename(columns={0: "Redacted_String", 1: "Token_Location", 2: "Redaction_Type"})
+            print("\n\n")
+            print("All redactions from: " + simpleFileName)
+            print(mystats)
+            print("Summary of: " + simpleFileName)
+            if mystats.empty:
+                print("No redactions made in this document.")
+            else:
+                print(mystats.groupby('Redaction_Type').count()[['Redacted_String']])
+
+
+def outputDoc(fileNames, foldername = "output/"):
     pass
 
-
-
- #def outputDoc(fileNames, foldername = "output/"):
-
-    #I should build a test here to make sure foldername is of the form folder/.
-
-
-    #for i in range(0,len(fileNames)):
-       # filename = os.path.basename(fileNames[i])
-        #pathname = os.path.abspath()
-
-
-         #report = open(foldername + 'redactedDocs.redacted', 'w')
-         #report.write()
-    # pass
-
-def runStats():
-    pass
 
 def redact(documents):
 
@@ -225,13 +270,22 @@ def redact(documents):
     # When redacting I can check if token.end[i] == token.start[i+1], these are uninterrupted redaction blocks.
     # Turn the whole thing (could be as large as a whole sentence) into one single block. "\u2588"
 
+    if not myRedactList:
+        for i in range (0,len(documents)):
+            doc = nlp(documents[i])
+            myRedactedDocs.append(doc)
+        return
 
     redactMe = []
     for i in range(0, len(myRedactList)):
         fileRedactList = myRedactList[i]
         redactTemp = sorted(fileRedactList, key=lambda start_token: start_token[1][0])
         redactMe.append(redactTemp)
-    print(redactMe)
+
+    myRedactList.clear()
+    for i in range(0,len(redactMe)):
+        myRedactList.append(redactMe[i])
+
     for i in range(0,len(documents)):
         doc = nlp(documents[i])
         redact_positions = []
@@ -241,37 +295,44 @@ def redact(documents):
         # say token 323:324 and 324:329 are both found, then we end up with just 323:329.
         j = 0
         while(j < len(redactMe[i])):
-            redact_start = redactMe[i][j][1][0] #Just a regular int at this point.
-            redact_end = redactMe[i][j][1][1]
+            redact_start = redactMe[i][j][1][0] #Just a regular int at this point. Doc i, term j's start token.
+            redact_end = redactMe[i][j][1][1] #Doc i, term j's stop token.
             chunk_resolved = False
             while not chunk_resolved:
                 if j < len(redactMe[i])-1 and redact_end == redactMe[i][j+1][1][0]:
                     redact_end = redactMe[i][j+1][1][1]
+                    j = j + 1
+                elif j < len(redactMe[i])-1 and redact_end > redactMe[i][j+1][1][0]: #In the case of an overlap, previous end will be larger than next start.
                     j = j + 1
                 else:
                     chunk_resolved = True
             redact_positions.append([redact_start,redact_end])
             j = j + 1
 
-        #From here, I can use my redact_start and redact_end as my banned token spans to reconstruct the document.
-
-
-        write_start = 0
-        write_end = redact_positions[0][0] #This is the first banned token position. Might be 0, that's ok.
-        for j in range(0,len(redact_positions)):
-            mydoc = mydoc + doc[write_start:write_end].text_with_ws
-            mydoc = mydoc + "\u2588 " #Single block for the entire redacted section will do, highly protected this way.
-
-            write_start = redact_positions[j][1] #This is the previous redact_end.
-            if j == len(redact_positions)-1:
-                break
-            else:
-                write_end = redact_positions[j+1][0] #This is the next redact_start. Basically, write_start:write_end spans the non-banned section.
-
-        if redact_positions[len(redact_positions)-1][1] < len(doc):
-            # We need to write one more time if the last token isn't on the redaction list.
+        if not redactMe[i]:
+            write_start = 0
             write_end = len(doc)
             mydoc = mydoc + doc[write_start:write_end].text_with_ws
+
+        else:
+            #From here, I can use my redact_start and redact_end as my banned token spans to reconstruct the document.
+
+            write_start = 0
+            write_end = redact_positions[0][0] #This is the first banned token position. Might be 0, that's ok.
+            for j in range(0,len(redact_positions)):
+                mydoc = mydoc + doc[write_start:write_end].text_with_ws
+                mydoc = mydoc + "\u2588 " #Single block for the entire redacted section will do, highly protected this way.
+
+                write_start = redact_positions[j][1] #This is the previous redact_end.
+                if j == len(redact_positions)-1:
+                    break
+                else:
+                    write_end = redact_positions[j+1][0] #This is the next redact_start. Basically, write_start:write_end spans the non-banned section.
+
+            if redact_positions[len(redact_positions)-1][1] < len(doc):
+                # We need to write one more time if the last token isn't on the redaction list.
+                write_end = len(doc)
+                mydoc = mydoc + doc[write_start:write_end].text_with_ws
 
 
         myRedactedDocs.append(mydoc)
@@ -279,13 +340,30 @@ def redact(documents):
 
 if __name__ == '__main__':
 
+    #KEY PIECE OF ARGPARSE CODE! parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
+    #From https://stackoverflow.com/questions/26727314/multiple-files-for-one-argument-in-argparse-python-2-7
+
+    # YAY, take a look at this website for --input a --input b to be converted to a list:
+    # https://docs.python.org/2/library/argparse.html#action
+    # 'append' - This stores a list, and appends each argument value to the list. This is useful to allow an option to be specified multiple times. Example usage:
+    # >>> parser = argparse.ArgumentParser()
+    # >>> parser.add_argument('--foo', action='append')
+    # >>> parser.parse_args('--foo 1 --foo 2'.split())
+    # Namespace(foo=['1', '2'])
+
 
     findNamesFlag = True
     findGendersFlag = False
+    findDatesFlag = True
+    findConceptFlag = True
+    conceptWord1 = "politics"
+    conceptWord2 = "politic"
+    fileext = "*.txt"   #need to update findDocs to accept folder inputs as well, like "cooldocs/*.txt"
 
-    fileext = "*.txt"
+
+
     findDocs(fileext)
-    print(myFileNames)
+
     unredactedDocs = []
 
     if(myFileNames):
@@ -295,15 +373,22 @@ if __name__ == '__main__':
             myFile1.close()
 
     myRedactList = []
+
     findNames(unredactedDocs, findNamesFlag, False)
-
-
     findGenders(unredactedDocs, findGendersFlag, findNamesFlag)
-    print(myRedactList)
+    findDates(unredactedDocs, findDatesFlag)
+    findConcepts(unredactedDocs, conceptWord1, findConceptFlag)
+    findConcepts(unredactedDocs, conceptWord2, findConceptFlag)
+
     redact(unredactedDocs)
-    print(myRedactedDocs[1])
+
+    print(myRedactList)
+
+    runStats("stdout",myFileNames)
 
 
     #for x in range(0,len(myRedactedDocs)):
-    #    print(myRedactedDocs[x])
+    print(myRedactedDocs[1])
+
+
 
